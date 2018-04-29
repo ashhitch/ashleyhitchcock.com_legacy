354b85c52e62d3861de4f2de275be36cf37e3238
Ash Hitchcock <hello@ashleyhitchcock.co.uk>
2018-04-29 20:04:38 +0100

    first commit

---
+++ frontend/config.js
@@ -0,0 +1,1 @@
+export const Config = {apiUrl: 'http://local.ashleyhitchcock.com'}
---
+++ frontend/pages/index.js
@@ -0,0 +1,84 @@
+import React, { Component } from "react";
+
+import { Config } from "../config.js";
+import Layout from "../components/Layout.js";
+import Link from "next/link";
+import Menu from "../components/Menu.js";
+import PageWrapper from "../components/PageWrapper.js";
+import fetch from "isomorphic-unfetch";
+
+const headerImageStyle = {
+    marginTop: 50,
+    marginBottom: 50
+};
+
+class Index extends Component {
+    static async getInitialProps(context) {
+        const pageRes = await fetch(
+            `${Config.apiUrl}/wp-json/postlight/v1/page?slug=welcome`
+        );
+        const page = await pageRes.json();
+        const postsRes = await fetch(
+            `${Config.apiUrl}/wp-json/wp/v2/posts?_embed`
+        );
+        const posts = await postsRes.json();
+        const pagesRes = await fetch(
+            `${Config.apiUrl}/wp-json/wp/v2/pages?_embed`
+        );
+        const pages = await pagesRes.json();
+        return { page, posts, pages };
+    }
+
+    render() {
+        const posts = this.props.posts.map((post, index) => {
+            return (
+                <ul key={index}>
+                    <li>
+                        <Link
+                            as={`/post/${post.slug}`}
+                            href={`/post?slug=${post.slug}&apiRoute=post`}
+                        >
+                            <a>{post.title.rendered}</a>
+                        </Link>
+                    </li>
+                </ul>
+            );
+        });
+        const pages = this.props.pages.map((page, index) => {
+            return (
+                <ul key={index}>
+                    <li>
+                        <Link
+                            as={`/page/${page.slug}`}
+                            href={`/post?slug=${page.slug}&apiRoute=page`}
+                        >
+                            <a>{page.title.rendered}</a>
+                        </Link>
+                    </li>
+                </ul>
+            );
+        });
+        return (
+            <Layout>
+                <Menu menu={this.props.headerMenu} />
+                <img
+                    src="/static/images/wordpress-plus-react-header.png"
+                    width="815"
+                    style={headerImageStyle}
+                />
+                <h1>{this.props.page.title ? this.props.page.title.rendered : null}</h1>
+                <div
+                    dangerouslySetInnerHTML={{
+                        __html: this.props.page.content.rendered ? this.props.page.content.rendered : null
+                    }}
+                />
+                <h2>Posts</h2>
+                {posts}
+                <h2>Pages</h2>
+                {pages}
+            </Layout>
+        );
+    }
+}
+
+export default PageWrapper(Index);
