5579db2306b8ccf873da1bd12806257e675a5723
Ash Hitchcock <hello@ashleyhitchcock.co.uk>
2018-11-05 08:54:28 +0000

    disable SW

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -45,12 +45,12 @@ class Index extends Component {
     }
 
     componentDidMount = () => {
-        if ("serviceWorker" in navigator) {
-            navigator.serviceWorker.register("/sw.js")
-                .catch(err => console.error("Service worker registration failed", err));
-        } else {
-            console.log("Service worker not supported");
-        }
+        // if ("serviceWorker" in navigator) {
+        //     navigator.serviceWorker.register("/sw.js")
+        //         .catch(err => console.error("Service worker registration failed", err));
+        // } else {
+        //     console.log("Service worker not supported");
+        // }
     }
 
     render() {
