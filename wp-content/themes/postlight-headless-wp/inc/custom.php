2da98fd999631f13de4704843fc3e57df9a824df
Ash Hitchcock <hello@ashleyhitchcock.co.uk>
2018-11-08 17:36:27 +0000

    WP API changes

--- wp-content/themes/postlight-headless-wp/inc/custom.php
+++ wp-content/themes/postlight-headless-wp/inc/custom.php
@@ -12,3 +12,25 @@ function ag_filter_post_json($response, $post, $context) {
 }
 
 add_filter( 'rest_prepare_post', 'ag_filter_post_json', 10, 3 );
+
+
+// w
+
+//add_action( 'rest_api_init', 'my_register_route' );
+
+function my_register_route() {
+    register_rest_route( 'contact/v1', 'send', array(
+                    'methods' => 'POST',
+                    'callback' => 'send_email',
+                    'args' => array(
+                      'email' => array_merge( $form_email_arg, array( 'required' => true ) ),
+                      'subject' => array_merge( $form_subject_arg, array( 'required' => true ) ),
+                      'message' => array_merge( $form_message_arg, array( 'required' => true ) )
+                    )
+                )
+            );
+}
+function send_email($request) {
+      wp_mail($request['email'], $request['subject'], $request['message']);
+    return rest_ensure_response( 'Hello World! This is my first REST API' );
+}
