b4657b27374633b41ef7f8aff3b9b829f507824a
Ash Hitchcock <hello@ashleyhitchcock.co.uk>
2019-01-06 20:31:28 +0000

    starting to convert to apollo

--- frontend/pages/blog.tsx
+++ frontend/pages/blog.tsx
@@ -1,36 +1,58 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
-import { Config } from "../config";
-import Grid from "../components/Grid";
-import Layout from "../components/Layout";
-import PageWrapper from "../components/PageWrapper";
-import fetch from "isomorphic-unfetch";
+import ErrorMessage from '../components/ErrorMessage';
+import Grid from '../components/Grid';
+import Layout from '../components/Layout';
+import Loader from '../components/Loader';
+import PageWrapper from '../components/PageWrapper';
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
 
+export const BLOG_QUERY = gql`
+  query home($slug: String!) {
+    posts(first: 4) {
+      edges {
+        node {
+          id
+          title
+          link
+          slug
+          date
+          content
+          excerpt
+          tags {
+            nodes {
+              name
+            }
+          }
+        }
+      }
+    }
+  }
+`;
 class Work extends Component {
-    IntroSection: any;
-    props: any;
-    constructor(props) {
-        super(props);
-    }
+  render() {
+    return (
+      <>
+        <Query query={BLOG_QUERY}>
+          {({ error, loading, data }) => {
+            if (error) return <ErrorMessage error={error} />;
+            if (loading) return <Loader />;
+            if (!data.posts) return <p>No Data returned</p>;
 
-    static async getInitialProps(context) {
+            const posts = data.posts.edges;
 
-        const postsRes = await fetch(
-            `${Config.apiUrl}/wp-json/wp/v2/posts?_embed&per_page=10`
-        );
-        const posts = await postsRes.json();
-        return {posts};
-    }
-
-    render() {
-
-        return (
-            <Layout>
+            return (
+              <Layout>
                 <h2>The blog</h2>
-                <Grid cards={this.props.posts} />
-            </Layout>
-        );
-    }
+                <Grid cards={posts} />
+              </Layout>
+            );
+          }}
+        </Query>
+      </>
+    );
+  }
 }
 
 export default PageWrapper(Work);
