ee1fed15ea270bf69da8150855cabc3dc824491b
Ash Hitchcock <hello@ashleyhitchcock.co.uk>
2019-01-11 21:21:21 +0000

    paging

--- frontend/pages/blog.tsx
+++ frontend/pages/blog.tsx
@@ -6,25 +6,43 @@ import { Heading } from './../components/styles/Headings';
 import Layout from './../components/Layout';
 import Loader from './../components/Loader';
 import PageWrapper from './../components/PageWrapper';
+import Pagination from './../components/Pagination';
 import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
 
 export const BLOG_QUERY = gql`
-{
-  posts: posts(first: 7) {
-    edges {
-      node {
-        id
-        title
-        link
-        slug
-        date
-        content
-        excerpt
+  query posts($cursor: String) {
+    posts: posts(first: 7, after: $cursor) {
+      pageInfo {
+        startCursor
+        endCursor
+        hasNextPage
+        hasPreviousPage
+      }
+      edges {
+        cursor
+        node {
+          id
+          title
+          slug
+          excerpt
+          featuredImage {
+            mediaDetails {
+              sizes {
+                name
+                file
+              }
+            }
+          }
+          tags {
+            nodes {
+              name
+            }
+          }
+        }
       }
     }
   }
-}
 `;
 class Work extends Component {
   render() {
@@ -37,11 +55,12 @@ class Work extends Component {
             if (!data.posts) return <p>No Data returned</p>;
 
             const posts = data.posts.edges;
-
+            const pageInfo = data.posts.pageInfo;
             return (
               <Layout>
                 <Heading>The blog</Heading>
                 <Grid cards={posts} linkType="post" />
+                <Pagination page={pageInfo} />
               </Layout>
             );
           }}
