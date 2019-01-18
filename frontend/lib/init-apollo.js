ce78e696dfb4fd0a1d3efed1bf3f286c37224702
Ash Hitchcock <hello@ashleyhitchcock.co.uk>
2019-01-18 13:34:06 +0000

    more state work

--- frontend/lib/init-apollo.js
+++ frontend/lib/init-apollo.js
@@ -1,15 +1,12 @@
-import {
-  ApolloClient,
-  HttpLink,
-  InMemoryCache
-} from 'apollo-boost'
 import { defaults, resolvers, typeDefs } from './../state/resolvers';
 import {
   endpoint,
   prodEndpoint
 } from './../config';
 
+import ApolloClient from 'apollo-boost'
 import fetch from 'isomorphic-unfetch'
+import withApollo from 'next-with-apollo';
 
 let apolloClient = null;
 
@@ -19,40 +16,15 @@ if (!process.browser) {
 }
 
 
-// const link = withClientState({ resolvers, defaults, cache, typeDefs });
+function create({ headers }) {
 
-function create(initialState) {
-  // Check out https://github.com/zeit/next.js/pull/4611 if you want to use the AWSAppSyncClient
   return new ApolloClient(
     {
-    //uri: process.env.NODE_ENV === 'development' ? endpoint : prodEndpoint,
-    connectToDevTools: process.browser,
-    ssrMode: !process.browser, // Disables forceFetch on the server (so queries are only run once)
-    link: new HttpLink({
-      uri: process.env.NODE_ENV === 'development' ? endpoint : prodEndpoint, // Server URL (must be absolute)
-      credentials: 'same-origin' // Additional fetch() options like `credentials` or `headers` same-origin
-    }),
-    cache: new InMemoryCache().restore(initialState || {}),
-    clientState: {
-      resolvers,
-      defaults,
-      typeDefs
-    }
+    uri: process.env.NODE_ENV === 'development' ? endpoint : prodEndpoint,
+    clientState: { resolvers, defaults, typeDefs },
+    credentials: 'same-origin'
   }
   )
 }
 
-export default function initApollo(initialState) {
-  // Make sure to create a new client for every server-side request so that data
-  // isn't shared between connections (which would be bad)
-  if (!process.browser) {
-    return create(initialState)
-  }
-
-  // Reuse client on the client-side
-  if (!apolloClient) {
-    apolloClient = create(initialState)
-  }
-
-  return apolloClient
-}
+export default withApollo(create);
