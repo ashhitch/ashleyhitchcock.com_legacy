44ca3aa6ab90dc49e7b42c408627256663ea0c4f
Ash Hitchcock <hello@ashleyhitchcock.co.uk>
2019-01-25 21:28:04 +0000

    styling

--- frontend/components/styles/Intro.ts
+++ frontend/components/styles/Intro.ts
@@ -1,3 +1,4 @@
+import media from './media';
 import styled from 'styled-components';
 
 const IntroWrap = styled.div`
@@ -5,8 +6,9 @@ const IntroWrap = styled.div`
   padding: 0;
   line-height: 1.4;
   margin-top: 2rem;
+  font-size: 1.125rem;
+  ${media.lg`font-size: 1.25rem;`}
 
-  
   h1 {
     margin: 0 0 1rem 0;
   }
