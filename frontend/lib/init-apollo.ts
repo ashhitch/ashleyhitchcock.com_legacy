e379c543774410c19e27db46b6eeb2bed0523ba6
Ash Hitchcock <hello@ashleyhitchcock.co.uk>
2019-01-28 20:08:34 +0000

    linting

---
+++ frontend/lib/init-apollo.ts
@@ -0,0 +1,27 @@
+import { defaults, resolvers, typeDefs } from "../state/resolvers";
+import { endpoint, prodEndpoint } from "../config";
+
+import ApolloClient from "apollo-boost";
+import fetch from "isomorphic-unfetch";
+import withApollo from "next-with-apollo";
+
+// Polyfill fetch() on the server (used by apollo-client)
+if (!process.browser) {
+  global.fetch = fetch;
+}
+
+function create({ headers }) {
+  const client = {
+    uri: process.env.NODE_ENV === "development" ? endpoint : prodEndpoint,
+    clientState: {
+      resolvers,
+      defaults,
+      typeDefs
+    },
+    credentials: "same-origin"
+  };
+
+  return new ApolloClient(client);
+}
+
+export default withApollo(create);
