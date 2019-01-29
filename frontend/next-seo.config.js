de3be593c6dfd60e44b26fe7ed5f660acbb565db
Ash Hitchcock <hello@ashleyhitchcock.co.uk>
2019-01-29 17:54:48 +0000

    linting

--- frontend/next-seo.config.js
+++ frontend/next-seo.config.js
@@ -1,12 +1,12 @@
 export default {
   openGraph: {
-    type: 'website',
-    locale: 'en_GB',
-    url: 'https://www.ashleyhitchcock.com',
-    site_name: 'Ash Hitchcock',
+    type: "website",
+    locale: "en_GB",
+    url: "https://www.ashleyhitchcock.com",
+    site_name: "Ash Hitchcock"
   },
   twitter: {
-    handle: '@ash_hitchcock',
-    cardType: 'summary_large_image',
-  },
+    handle: "@ash_hitchcock",
+    cardType: "summary_large_image"
+  }
 };
