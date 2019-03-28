b859c07b0054c3241f25e684c179e0238c5d7d9b
Ash Hitchcock <ashley.hitchcock@freshegg.com>
2019-03-28 20:31:11 +0000

    cards and clients

--- frontend/components/Techstack.tsx
+++ frontend/components/Techstack.tsx
@@ -1,54 +1,13 @@
-import React, { Component } from 'react';
-
-import styled from 'styled-components';
+import React from 'react';
+import { IconGridStyle, IconGridItemStyle } from './styles/IconGrid';
 import AngularIcon from './icons/angular.svg';
 import CssIcon from './icons/css.svg';
-import IntroWrap from './styles/Intro';
+
 import JavascriptIcon from './icons/javascript.svg';
 import NodeIcon from './icons/node.svg';
 import ReactIcon from './icons/react.svg';
 import { SubHeading } from './styles/Headings';
 import WordPressIcon from './icons/wordpress.svg';
-import media from './styles/media';
-
-const IconGridStyle = styled.div`
-  display: grid;
-  width: 100%;
-  padding: 0;
-  list-style-type: none;
-  grid-template-columns: repeat(auto-fill, minmax(120px, 1fr));
-  grid-gap: 2.25rem;
-  justify-items: center;
-  align-items: center;
-  ${media.md`grid-template-columns: repeat(auto-fill, minmax(180px, 1fr));  grid-gap: 4rem;`}
-  ${media.lg`grid-template-columns: repeat(auto-fill, minmax(280px, 1fr));`}
-`;
-const IconGridItemStyle = styled.div`
-  text-align: center;
-
-  svg {
-    width: 100%;
-    max-width: 140px;
-    height: auto;
-    ${media.md`max-width: 180px;`}
-  }
-  svg {
-    transition: transform 0.2s ease-in;
-
-    path,
-    circle {
-      fill: ${props => props.theme.primary};
-      transition: fill 0.2s ease-in;
-    }
-  }
-  &:hover svg {
-    transform: scale(1.1);
-  }
-  &:hover svg path,
-  &:hover svg circle {
-    fill: ${props => props.brandbg};
-  }
-`;
 
 const Techstack = () => (
   <>
